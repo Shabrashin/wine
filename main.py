24046658754467deba78d747ceabafb83c2f71be
Shabrashin <IvanShabrashin@gmail.com>
2022-04-25 14:36:14 +0300

    Version 2

--- main.py
+++ main.py
@@ -1,38 +1,55 @@
+import argparse
+import os
 from calendar import c
-from http.server import HTTPServer, SimpleHTTPRequestHandler
-from jinja2 import Environment, FileSystemLoader, select_autoescape
-from datetime import datetime
 from collections import defaultdict
+from datetime import datetime
+from email.policy import default
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+
 import pandas
-import pprint
+from dotenv import load_dotenv
+from jinja2 import Environment, FileSystemLoader, select_autoescape
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
+COMPANY_ESTABLISHMENT_YEAR = 1920
 
-template = env.get_template('template.html')
+def main():
+    load_dotenv()
 
-excel_data = pandas.read_excel(
-    io='wine3.xlsx', 
-    sheet_name='Лист1',
-    na_values=['N/A', 'NA'],
-    keep_default_na=False
-).to_dict(orient='records')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data', help='Введите путь к файлу с данными', default=os.getenv('DATA_PATH'))
+    args = parser.parse_args()
+    data = args.data
+
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml']),
+    )
+    template = env.get_template('template.html')
+
+    wine_file_data = pandas.read_excel(
+        io=data, 
+        sheet_name='Лист1',
+        na_values=['N/A', 'NA'],
+        keep_default_na=False
+    ).to_dict(orient='records')
 
 
-collection = defaultdict(list)
+    wine_collection = defaultdict(list)
 
-for wine in excel_data:
-    collection[wine['Категория']].append(wine)
+    for wine in wine_file_data:
+        wine_collection[wine['Категория']].append(wine)
 
-rendered_page = template.render(
-    year=datetime.now().year - 1920,
-    collection=collection,
-)
+    rendered_page = template.render(
+        year=datetime.now().year - COMPANY_ESTABLISHMENT_YEAR,
+        wine_collection=wine_collection,
+    )
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
+
+
+if __name__ == '__main__':
+    main()
